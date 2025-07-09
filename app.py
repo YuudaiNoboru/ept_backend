e6b00065d9e6f775b06d2edc509198d6733640ee
Lucas Jose Santos Vasconcelos <lucas.vasconcelos@codevasf.gov.br>
2025-07-09 17:41:15 -0300

    Implementação do CORSMiddlwware para utilziação do frontend.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.main import api_router
 from core.settings import settings
@@ -6,6 +7,17 @@ from core.settings import settings
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f'{settings.API_V1_STR}/openapi.json',
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        'http://localhost:9000',
+        'http://localhost:8080'
+    ],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
