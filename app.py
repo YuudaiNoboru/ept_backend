f909a5818e046230088f0c185a04896c7dae43ab
Lucas Jose Santos Vasconcelos <lucas.vasconcelos@codevasf.gov.br>
2025-07-10 17:33:51 -0300

    Correção de bugs.

--- app.py
+++ app.py
@@ -11,10 +11,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        'http://localhost:9000',
-        'http://localhost:8080'
-    ],
+    allow_origins=['http://localhost:9000', 'http://localhost:8080'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
